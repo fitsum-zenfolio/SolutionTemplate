bf3a3f7977eac013b830e4f19b5738e1cd9adc1d
Tony Wilsman <tony.wilsman@ceralytics.com>
2020-04-10 10:00:10 -0500

    Simplified config class.  Removed cruft.  Added developer-specific loading of config files

--- templates/dplsln/DPL.Template.Accessors.Shared/EntityFramework/partials/DatabaseContext.cs
+++ templates/dplsln/DPL.Template.Accessors.Shared/EntityFramework/partials/DatabaseContext.cs
@@ -1,64 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System;
 
 namespace DPL.Template.Accessors.Shared.EntityFramework
 {
     public partial class DatabaseContext : DbContext
     {
-        public bool AllowDispose { get; set; } = true;
-
-        internal static DatabaseContext UnitTestContext { get; set; }
-
-        // Everyone that uses the DatabaseContext will use this 
-        // constructor method
-        internal static DatabaseContext Create(bool allowDispose = true)
-        {
-            if (UnitTestContext == null)
-            {
-                return new DatabaseContext { AllowDispose = allowDispose };
-            }
-
-            return UnitTestContext;
-        }
-
-        public override void Dispose()
-        {
-            // this is the secret of the wrapper, without this do nothing we won't handle rolling back transactions
-            // only dispose if we are allowing it to dispose
-            if (AllowDispose)
-            {
-                base.Dispose();
-            }
-        }
-
         protected IConfigurationRoot Configuration { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var builder = new ConfigurationBuilder()
-                .AddEnvironmentVariables();
-
-            Configuration = builder.Build();
-
             base.OnConfiguring(optionsBuilder);
-
-            var connectionString = Common.Shared.Config.SqlServerConnectionString;
-
-            if (!string.IsNullOrEmpty(connectionString))
+            if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer(connectionString);
-            }
-            else
-            {
-                connectionString = Common.Shared.Config.SqliteConnectionString;
-
-                if (string.IsNullOrEmpty(connectionString))
-                {
-                    throw new InvalidOperationException("Connection string environment variable missing.");
-                }
-
-                optionsBuilder.UseSqlite(connectionString);
+                optionsBuilder.UseSqlServer(Common.Shared.Config.SqlServerConnectionString);
             }
         }
     }
